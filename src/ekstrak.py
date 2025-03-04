3a252b65a4fbc52cacbae86761cf30e2b4c93e5b
rikin <rikinugraha@student.telkomuniversity.ac.id>
2025-03-04 15:50:09 +0700

    fix ETL

--- src/ekstrak.py
+++ src/ekstrak.py
@@ -2,42 +2,6 @@ import pandas as pd
 import glob
 import os
 
-# def extract_data(folder_path):
-#     try:
-#         csv_files = glob.glob(os.path.join(folder_path, "csv/*.csv"))
-#         json_files = glob.glob(os.path.join(folder_path, "json/*.json"))
-
-#         if not csv_files and not json_files:
-#             print("Tidak ada file CSV atau JSON yang ditemukan.")
-#             return None
-
-#         df_list = []
-
-#         for file in csv_files:
-#             print(f"[INFO] Membaca file CSV: {file}")
-#             df = pd.read_csv(file)
-#             df_list.append(df)
-
-#         for file in json_files:
-#             print(f"[INFO] Membaca file JSON: {file}")
-#             df = pd.read_json(file)
-#             df_list.append(df)
-
-#         df_combined = pd.concat(df_list, ignore_index=True)
-
-#         os.makedirs("staging", exist_ok=True)
-#         staging_csv = "staging/staged_data.csv"
-
-#         df_combined.to_csv(staging_csv, index=False)
-
-#         print(f"[INFO] Data berhasil disimpan di {staging_csv}")
-
-#         return df_combined
-    
-#     except Exception as e:
-#         print(f"Terjadi kesalahan: {e}")
-#         return None
-
 
 class Extractor:
     def __init__(self, folder_path):
--- src/transform.py
+++ src/transform.py
@@ -2,63 +2,6 @@ import pandas as pd
 import re
 
 
-# def calculate_trip_duration(df, start_col, end_col):
-#     print('Menambahkan kolom trip durasi')
-#     df = df.copy()
-#     df[end_col] = pd.to_datetime(df[end_col])
-#     df[start_col] = pd.to_datetime(df[start_col])
-    
-#     df['trip_duration'] = (df[end_col] - df[start_col]).astype(str).str.split().str[-1]
-
-#     df[start_col] = df[start_col].dt.strftime('%Y-%m-%d %H:%M:%S')
-#     df[end_col] = df[end_col].dt.strftime('%Y-%m-%d %H:%M:%S')
-    
-#     print('Kolom trip durasi berhasil ditambahkan\n')
-    
-#     return df
-
-# # ============================================
-
-# def to_snake_case(name):
-#     name = re.sub(r'([a-z])([A-Z])', r'\1_\2', name)  
-#     name = re.sub(r'([A-Z]+)([A-Z][a-z])', r'\1_\2', name)  
-#     name = re.sub(r'[\s\W]+', '_', name)
-#     return name.lower().strip('_') 
-
-# # ===========================================
-
-# def map_payment_types(df, df_mapping, key_col, value_col):
-#     print('Merubah semua nilai payment_type menjadi label')
-#     df = df.copy()
-#     payment_mapping = dict(zip(df_mapping[key_col], df_mapping[value_col]))
-#     df['payment_type'] = df['payment_type'].map(payment_mapping)
-#     print('Semua nilai payment_type berhasil menjadi label\n')
-#     return df
-
-# # ===============================================
-
-# def convert_distance_to_km(df, distance_col):
-#     print('Merubah satuan trip_distance menjadi km')
-#     df = df.copy()
-#     df[distance_col] = (df[distance_col] * 1.60934).round(2)
-#     print('Satuan trip_distance berhasil dirubah menjadi km\n')
-#     return df
-
-# # =============================================
-
-# def load_data(df):
-#     save_format = input("Pilih format penyimpanan (csv/excel): ").strip().lower()
-#     if save_format == "csv":
-#         df.to_csv("./result/result_data.csv", index=False)
-#         print("Data berhasil disimpan sebagai CSV.")
-#     elif save_format == "excel":
-#         df.to_excel("./result/result_data.xlsx", index=False)
-#         print("Data berhasil disimpan sebagai Excel.")
-#     else:
-#         print("Format tidak dikenali. Silakan pilih antara 'csv' atau 'excel'.")
-
-
-
 class Transformer:
     @staticmethod
     def calculate_trip_duration(df, start_col, end_col):
@@ -95,6 +38,7 @@ class Transformer:
         df[distance_col] = (df[distance_col] * 1.60934).round(2)
         print('Satuan trip_distance berhasil dirubah menjadi km\n')
         return df
+
 
 class Loader:
     @staticmethod
